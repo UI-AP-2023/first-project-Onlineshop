7c26e933f12d5389a6310d78c9befd5af796373e
Shabnam2003 <khaghanpourshabnam@gmail.com>
2023-05-23 15:33:13 +0430

    add circleChance function and add date to Main.

---
+++ view/CreditCardPage.java
@@ -0,0 +1,54 @@
+package view;
+
+import controller.user.BasketController;
+import model.exceptions.InvalidInformationExceptions;
+import model.users.Customer;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class CreditCardPage {
+    private Scanner scanner;
+    UserPage userPage = new UserPage();
+
+    public CreditCardPage() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void charging(Customer onlineCustomer) {
+
+        System.out.println("Please enter your credit card's number:");
+        String number = scanner.next();
+        System.out.println("Please enter your credit card's Cvv2:");
+        String cvv2 = scanner.next();
+        System.out.println("Please enter your credit card's password:");
+        String password = scanner.next();
+        System.out.println("Please enter money:");
+        double money = scanner.nextDouble();
+
+        BasketController basketController = new BasketController();
+        try {
+            basketController.chargeRequest(onlineCustomer, number, cvv2, password, money);
+            System.out.println("Your request has been sent.");
+            userPage.menu();
+        } catch (InvalidInformationExceptions invalidInformationExceptions) {
+            System.out.println(invalidInformationExceptions.toString());
+            System.out.println("If you want to exit enter '0' and if you want to try again enter '10':");
+            int answerExit =-1;
+            try {
+                answerExit = scanner.nextInt();
+            }catch (InputMismatchException inputMismatchException){
+                scanner.nextLine();
+                charging(onlineCustomer);
+            }
+            if (answerExit == 0) {
+                userPage.menu();
+            } else if (answerExit == 10) {
+                charging(onlineCustomer);
+            }
+
+        }finally {
+            System.out.println("Good Luck!");
+        }
+    }
+}
---
+++ view/SignupPage.java
@@ -0,0 +1,84 @@
+package view;
+
+import controller.user.UserController;
+import model.exceptions.InvalidEmailExceptions;
+import model.exceptions.InvalidPassword;
+import model.exceptions.InvalidPhoneNumberExceptions;
+import model.exceptions.InvalidUserName;
+
+import java.util.Scanner;
+
+public class SignupPage {
+    private Scanner scanner;
+
+    public SignupPage() {
+        scanner = new Scanner(System.in);
+    }
+
+    Main main = new Main();
+
+    public void signupMenu() {
+        UserController userController = new UserController();
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("Enter your username(It must be unique)  'Enter 0 to exit'  :");
+        String username = scanner.nextLine();
+        if (username.equals("0")) {
+            main.mainPage();
+        }
+        try {
+            userController.setUserName(username);
+        } catch (InvalidUserName invalidUserName) {
+            System.out.println(invalidUserName.toString());
+            signupMenu();
+        }finally {
+            System.out.println("Good Luck!");
+        }
+        System.out.println("----------------------------------------------------------------");
+            System.out.println("Enter your password please(It must be more than 7)  'Enter 0 to exit'  :");
+            String password = scanner.nextLine();
+            if (password.equals("0")) {
+                main.mainPage();
+            }
+            try {
+                userController.setPassword(password);
+            }catch (InvalidPassword invalidPassword){
+                System.out.println(invalidPassword.toString());
+                signupMenu();
+            }finally {
+                System.out.println("Good Luck!");
+            }
+
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("Enter your phone number please(It must be signed up for one account)   'Enter 0 to exit' :");
+        String phoneNumber = scanner.nextLine();
+        if (phoneNumber.equals("0")) {
+            main.mainPage();
+        }
+        try {
+            userController.setPhoneNumber(phoneNumber);
+        } catch (InvalidPhoneNumberExceptions invalidPhoneNumberExceptions) {
+            System.out.println(invalidPhoneNumberExceptions.toString());
+            signupMenu();
+        }finally {
+            System.out.println("Good Luck!");
+        }
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("Enter your email address please(It must be signed up for one account)   'Enter 0 to exit' :");
+        String email = scanner.nextLine();
+        if (email.equals("0")) {
+            main.mainPage();
+        }
+        try {
+
+            userController.setEmail(email);
+        } catch (InvalidEmailExceptions invalidEmailExceptions) {
+            System.out.println(invalidEmailExceptions.toString());
+            signupMenu();
+        }finally {
+            System.out.println("Good Luck!");
+        }
+
+        userController.signupRequest(username, password, phoneNumber, email);
+        main.mainPage();
+    }
+}
