8356154db576788f0f4fd9ef25b2c1197e7dc6a0
Shabnam2003 <khaghanpourshabnam@gmail.com>
2023-05-29 01:10:58 +0430

    add all part of graphics.

--- Digikala/src/main/java/module-info.java
+++ Digikala/src/main/java/module-info.java
@@ -1,8 +1,6 @@
 module com.example.digikala {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
     opens com.example.digikala to javafx.fxml;
     exports com.example.digikala;
 }
